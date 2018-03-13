b1c6ba7ad7c9f92a59be57195e33522230f7099a
Evan Cheng <dr.frolicks@gmail.com>
2018-03-13 14:48:49 -0700

    -fixed bug where too many piles were generated for given money value in PileGenerator

--- Assets/Scripts/PileGenerator.cs
+++ Assets/Scripts/PileGenerator.cs
@@ -16,7 +16,8 @@ namespace CashSpawning
             while (spawnedMoney < money)
             {
                 float maxPileValue = CashPilePrefab.GetComponent<CashPile>().maxPileValue;
-                float pileMoney = money < maxPileValue ? money : maxPileValue;
+                float moneyLeftToSpawn = money - spawnedMoney; 
+                float pileMoney = moneyLeftToSpawn < maxPileValue ? moneyLeftToSpawn : maxPileValue;
                 spawnedMoney += pileMoney;
 
                 CashPile spawnedPile = Instantiate(CashPilePrefab, PileParent).GetComponent<CashPile>();
