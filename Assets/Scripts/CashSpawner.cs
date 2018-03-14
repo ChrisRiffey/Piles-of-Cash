4f14a83496e2ba0cb81e4000ef961af136babfce
Evan Cheng <dr.frolicks@gmail.com>
2018-03-14 15:53:03 -0700

    Lane-spawning tweaks

--- Assets/Scripts/CashSpawner.cs
+++ Assets/Scripts/CashSpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine.Events;
 
 public class CashSpawner : MonoBehaviour {
     public float distanceBetweenPiles;
-    public float stackSpawnInterval;  
+    public float stackSpawnInterval;
+
+    public int maxRow, maxCol;  
 
     [Header("Must be BIG to SMALL")]
     public GameObject[] stackDenominationPrefab;
@@ -20,11 +22,14 @@ public class CashSpawner : MonoBehaviour {
         if (stackSpawnIntervalWFS == null)
             stackSpawnIntervalWFS = new WaitForSeconds(stackSpawnInterval);
     }
-
+    private void Start()
+    {
+        spawnMoney(56500, GameObject.Find("TestProfile").transform);  
+    }
 
     public void spawnMoney(float amount, Transform startPosition)
     {
-        // 0 - 1.5 mil stacks, 1 - 100k, 2 - 10k, 3 - 5k, 4 - 1k, 5 - 100)
+        // 0 - 1.5 mil stack, 1 - 10k, 2 - 5k, 3 - 1k, 4 - 100)
         money = (int)amount; 
         int[] stacksToSpawn = parseNumOfStacksToSpawn(amount);
         StartCoroutine(stackSpawningCR(stacksToSpawn, startPosition.position)); 
@@ -46,22 +51,32 @@ public class CashSpawner : MonoBehaviour {
             Stack stackSettings = stackDenominationPrefab[prefabIndex].GetComponent<Stack>();
 
             int stacksSpawned = 0; 
-            for (int row = 0; row < 10 && stacksSpawned < stacksToSpawn[prefabIndex]; row++)
+            
+            for (int row = 0; row < maxRow; row++)
             {
-                for (int col = 0; col < 10 && stacksSpawned < stacksToSpawn[prefabIndex]; col++)
+                if (stacksSpawned >= stacksToSpawn[prefabIndex])
+                    break;
+
+                for (int col = 0; col < maxCol; col++)
                 {
+                    if (stacksSpawned >= stacksToSpawn[prefabIndex])
+                        break; 
+     
                     stacks[row][col] = Instantiate(stackDenominationPrefab[prefabIndex], spawnPosition, Quaternion.identity, stackParent.transform);
                     stacksSpawned++; 
 
                     if (stackSpawnInterval != 0f)
                         yield return stackSpawnIntervalWFS;
 
-                    spawnPosition += Vector3.right * stackSettings.worldDimensions.z;
+                    spawnPosition += Vector3.right * (stackSettings.worldDimensions.x + stackSettings.worldDimensions.x / 4);
                 }
-                spawnPosition = new Vector3(spawnPosition.x + stackSettings.worldDimensions.x, spawnPosition.y, startPosition.z);
+
+
+                spawnPosition = new Vector3(startPosition.x, startPosition.y, spawnPosition.z - stackSettings.worldDimensions.z);
             }
 
-            startPosition += direction * distanceBetweenPiles;  
+            if(stacksToSpawn[prefabIndex] != 0)
+                startPosition += direction * distanceBetweenPiles;  
         }
         OnSpawnComplete.Invoke();  
     }
@@ -69,8 +84,8 @@ public class CashSpawner : MonoBehaviour {
     int[] parseNumOfStacksToSpawn(float amount)
     {
 
-        int[] stackDenominations = { 1500000, 100000, 10000, 5000, 1000, 100 };
-        int[] stackCount = new int[6];
+        int[] stackDenominations = {1500000, 10000, 5000, 1000, 100};
+        int[] stackCount = new int[stackDenominations.Length];
 
         for (int i = 0; i < stackCount.Length; i++)
         {
--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
 public class Stack : MonoBehaviour {
     public float value;
     public Vector3 worldDimensions;
